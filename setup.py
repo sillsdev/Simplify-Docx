438ffefe2c40fa8d6f5a888aabbf9c51e6336e37
Jim Grady <jimgrady.jg@gmail.com>
2022-03-29 14:39:14 -0400

    Bump version to distinguish from PyPI install

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def read(*names, **kwargs):
 
 setup(
     name="simplify-docx",
-    version="0.1.1",
+    version="0.1.2",
     description="A utility for simplifying python-docx document objects",
     long_description="%s\n%s"
     % (
